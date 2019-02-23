f7d659e4d8963ffb94a44ddb4e9e398fb874afe5
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-02-23 14:36:54 +0100

    passwdgen

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App 
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kw):
+        super(LoginScreen, self).__init__(**kw)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+class MyApp(App):
+    def build(self):
+        return LoginScreen()
+
+MyApp().run()
---
+++ passwdgen.py
@@ -0,0 +1,24 @@
+import random
+LOWCASE = 'abcdefghijklmnopqrstuvwxyz'
+UPPERCASE = LOWCASE.upper()
+NUMERIC = '0123456789'
+SEPR = '_-'
+class Password(object):
+    label = ''
+    def __init__(self, count=8, upperCase=False, numeric=False, sepr=False):
+        self.count = count
+        self.upperCase = upperCase
+        self.numeric = numeric
+        self.sepr = sepr
+
+    def genPasswd(self):
+        gen = LOWCASE
+        if self.upperCase:
+            gen += UPPERCASE
+        if self.numeric:
+            gen += NUMERIC
+        if self.sepr:
+            gen += SEPR
+        gen = list(gen)
+        random.shuffle(gen)
+        self.label =  ''.join([random.choice(gen) for x in range(self.count)])
