a5ca4d5f4c619fe0fa12c584638102b1fb538974
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-02-25 21:55:18 +0100

    struct_kivy

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
+import passwdgen
 from kivy.app import App 
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.config import Config
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.checkbox import CheckBox
 
-class LoginScreen(GridLayout):
-    def __init__(self, **kw):
-        super(LoginScreen, self).__init__(**kw)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+Config.set('graphics', 'resizeable', '0')
+Config.set('graphics', 'width', '640')
+Config.set('graphics', 'height', '700')
 
-class MyApp(App):
+class PasswordGeneratorWidget(BoxLayout):
+    pass
+
+class PasswordManagerWidget(BoxLayout):
+    pass
+
+class SettingsWidget(BoxLayout):
+    pass
+
+class RootWidget(BoxLayout):
+    def clear(self):
+        self.clear_widgets()
+    def next_state(self, state):
+        self.ids.widg.clear_widgets()
+        if state == 'PasswordGeneratorWidget':
+            widg = PasswordGeneratorWidget()
+        elif state == 'PasswordManagerWidget':
+            widg = PasswordManagerWidget()
+        elif state == 'SettingsWidget':
+            widg = SettingsWidget()
+        self.ids.widg.add_widget(widg)
+    pass
+
+
+class PasswordGeneratorApp(App):
     def build(self):
-        return LoginScreen()
+        root = RootWidget()
+        return self.root
 
-MyApp().run()
+def __main__():
+    root = PasswordGeneratorApp()
+    root.run()
+
+if __name__ == '__main__':
+    __main__()
