0dd19335a8a51899811781f5788e8a21280e7c3b
kalalannn <39317136+kalalannn@users.noreply.github.com>
2019-02-27 23:37:33 +0100

    Version 1.2

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ class InformationWidget(BoxLayout):
         self.author = AnchorLayout(anchor_x='right', size_hint=(1, .3))
         self.rights = AnchorLayout(anchor_x='right', size_hint=(1, .3))
 
-        self.version.label = AboutLabel(text='version: 1.0')
+        self.version.label = AboutLabel(text='version: 1.2')
         self.author.label = AboutLabel(text='Author: Nikolaj Vorobiev')
         self.rights.label = AboutLabel(text='License: GNU GPLv3')
 
